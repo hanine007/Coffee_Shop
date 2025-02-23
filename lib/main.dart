99fc34d0547c3a1377396be0a0363e555d56c0c1
bouchibahanine <bouchibahanine@gmail.com>
2025-02-23 17:02:33 +0000

    add the index for the pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/offerpages.dart';
+import 'package:myapp/Orderpages.dart';
+import 'package:myapp/Menupages.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Coffee Shop',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Color.fromARGB(255, 96, 59, 45), // Couleur marron
+          seedColor: const Color.fromARGB(255, 96, 59, 45), // Marron principal
           brightness: Brightness.light, // Mode clair
         ),
         useMaterial3: true, // Active Material 3
@@ -32,33 +34,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int selectedIndex = 0;
+
+  // Liste des pages associées aux onglets
+  final List<Widget> pages = [
+    //Liste de pages[]
+    // const Center(child: Text("Menu Page", style: TextStyle(fontSize: 20))),
+    const Menupages(),
+    const Offerpages(),
+    const orderpages(),
+    //const Center(child: Text("Order Page", style: TextStyle(fontSize: 20))),
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:
-            Theme.of(
-              context,
-            ).colorScheme.primary, // Utilisation de la couleur principale
-        title: Image.asset(
-          'images/logo.png',
-          height: 40,
-        ), // Ajustement de la hauteur du logo si besoin
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Image.asset('images/logo.png', height: 40),
       ),
+      body: pages[selectedIndex], // Affiche la page correspondante
 
       bottomNavigationBar: BottomNavigationBar(
-        items: [
+        currentIndex: selectedIndex,
+        onTap: onTabTapped, // Met à jour l'onglet sélectionné
+        selectedItemColor: Colors.amber.shade400,
+        unselectedItemColor: Colors.brown.shade300,
+        items: const [
           BottomNavigationBarItem(label: "Menu", icon: Icon(Icons.coffee)),
-                    BottomNavigationBarItem(label: "Offers", icon: Icon(Icons.local_offer)),
-          BottomNavigationBarItem(label: "Order", icon: Icon(Icons.shopping_cart)),
-
+          BottomNavigationBarItem(
+            label: "Offers",
+            icon: Icon(Icons.local_offer),
+          ),
+          BottomNavigationBarItem(
+            label: "Order",
+            icon: Icon(Icons.shopping_cart),
+          ),
         ],
       ),
-      body: Offerpages(),
-
-      // const Center(
-      // child: Text("Bienvenue au Coffee Shop !"),
-      //),
     );
   }
 }
