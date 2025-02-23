0452a52784b62e72172e5e52dd454d84748c0369
bouchibahanine <bouchibahanine@gmail.com>
2025-02-23 17:02:58 +0000

    add pages

---
+++ lib/Menupages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Menupages extends StatelessWidget {
+  const Menupages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/Orderpages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class orderpages extends StatelessWidget {
+  const orderpages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
