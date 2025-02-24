8daede63766ffd08dfa237d208d38d1323953eb8
bouchibahanine <bouchibahanine@gmail.com>
2025-02-24 14:48:57 +0000

    json to classe

--- lib/datamodel.dart
+++ lib/datamodel.dart
@@ -11,12 +11,26 @@ class Product {
     required this.price,
     required this.image,
   });
+  // Convertit une Map en objet Product
+    factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+        id: json['id'] as int,
+        name: json['name'] as String,
+        price: json['price'] as double,
+        image: json['image'] as String);
+  }
+
 }
 
 class Category {
   String name;
   List<Product> products; // Liste des produits associés à la catégorie
   Category({required this.name, required this.products});
+ factory Category.fromJson(Map<String, dynamic> json) {
+    var productsJson = json['products'] as Iterable<dynamic>;
+    var products = productsJson.map((p) => Product.fromJson(p)).toList();
+    return Category(name: json['name'] as String, products: products);
+  }
 }
 
 class ItemClass {
